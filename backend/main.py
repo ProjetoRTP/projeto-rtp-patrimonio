179e19d908338155ec52d79a3175c96f52bde696
Kauã Santiago <kauaandrade1806@gmail.com>
2026-04-22 09:47:33 -0300

    Atualização nas rotas

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,16 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 from routes.register import register_routes
+from modules.user.routes import user_bp
+from modules.forgot.routes import forgot_bp
 
 
 def create_app():
     app = Flask(__name__)
     CORS(app)
     register_routes(app)
+    app.register_blueprint(user_bp)
+    app.register_blueprint(forgot_bp)
 
     @app.route("/")
     def hello():
