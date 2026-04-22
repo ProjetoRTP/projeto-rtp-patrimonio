37efa9fa68c22775752dcf5cd837b1109d947864
kauaS4ntiago <kauaandrade1806@gmail.com>
2026-04-21 21:45:03 -0300

    CRUD routes, authentication, and password recovery implemented.

--- backend/modules/auth/routes.py
+++ backend/modules/auth/routes.py
@@ -1,6 +1,65 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
+from flask_jwt_extended import (
+    create_access_token,
+    jwt_required,
+    get_jwt_identity
+)
+from datetime import timedelta
+from database.connection import get_connection
 
-auth_bp = Blueprint("auth", __name__)
+auth_bp = Blueprint("auth_bp", __name__, url_prefix="/auth")
 
-# TODO: POST /auth/login
-# TODO: POST /auth/logout
+
+@auth_bp.route("/login", methods=["POST"])
+def login():
+    dados = request.get_json()
+    cpf = dados.get("cpf")
+    senha = dados.get("senha")
+
+    if not cpf or not senha:
+        return jsonify({"erro": "Cpf e senha obrigatórios"}), 400
+
+    try:
+        conn = get_connection()
+        cursor = conn.cursor(dictionary=True)
+
+        cursor.execute("SELECT * FROM usuarios WHERE cpf=%s AND senha=%s AND ativo=TRUE", (cpf, senha))
+        usuario = cursor.fetchone()
+
+        if usuario:
+            access_token = create_access_token(
+                identity=usuario["id"],
+                additional_claims={"cargo": usuario["perfil"]},
+                expires_delta=timedelta(hours=1)
+            )
+            return jsonify({
+                "status": "sucesso",
+                "token": access_token,
+                "usuario": {
+                    "id": usuario["id"],
+                    "nome": usuario["nome"],
+                    "perfil": usuario["perfil"]
+                }
+            }), 200
+        else:
+            return jsonify({"erro": "Usuário ou senha inválidos"}), 401
+
+    except mysql.connector.Error as err:
+        return jsonify({"erro": f"Erro no banco: {err}"}), 500
+
+    finally:
+        if conn.is_connected():
+            cursor.close()
+            conn.close()
+
+@auth_bp.route("/logout", methods=["POST"])
+@jwt_required()
+def logout():
+    try:
+        usuario_id = get_jwt_identity()
+        return jsonify({
+            "status": "sucesso",
+            "mensagem": f"Logout realizado para usuário {usuario_id}"
+        }), 200
+    except Exception as e:
+        return jsonify({"erro": str(e)}), 400
--- backend/modules/forgot/routes.py
+++ backend/modules/forgot/routes.py
@@ -1,5 +1,89 @@
 from flask import Blueprint
+from flask_jwt_extended import jwt_required
+from flask import Blueprint, request, jsonify
+from datetime import timedelta, datetime
+from config. settings import *
+from functools import wraps
 
 forgot_bp = Blueprint("forgot", __name__)
 
-# TODO: recuperação de senha
+tokenSaved= {}
+
+import secrets
+import smtplib 
+from email.mime.text import MIMEText
+
+def create_token():
+    return secrets.token_urlsafe(6)
+
+def send_email(recipient, token):
+    sender = "rtpsistema@gmail.com"
+    password = "mjrodxblhwhjvfsh"
+
+    menssage = MIMEText(f"Seu token de recuperação de senha é: {token}")
+    menssage["Subject"] = "Recuperação de Senha"
+    menssage["From"] = sender
+    menssage["To"] = recipient
+
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(sender, password)
+    server.send_message(menssage)
+    server.quit()
+
+@forgot_bp.route('/send-token', methods=['POST'])
+def send_token():
+    dados = request.get_json()
+    email = dados.get("email")
+
+    if not email:
+        return jsonify({"erro": "Email obrigatório"}), 400
+
+    try:
+        user = user_obj.get_by_email(email)
+        if not user:
+            return jsonify({"erro": "Usuário não encontrado"}), 404
+
+        token = create_token()
+
+        tokenSaved[email] = {
+            "token": token,
+            "expira": datetime.now() + timedelta(minutes=10)
+        }
+
+        send_email(email, token)
+
+        return jsonify({"status": "Token enviado"}), 200
+
+    except Exception as e:
+        return jsonify({"erro": str(e)}), 400
+    
+@forgot_bp.route('/reset-password', methods=['PUT'])
+def reset_password():
+    dados = request.get_json()
+
+    email = dados.get("email")
+    token = dados.get("token")
+    new_password = dados.get("senha")
+
+    if not email or not token or not new_password:
+        return jsonify({"erro": "Dados incompletos"}), 400
+
+    registro = tokenSaved.get(email)
+
+    if not registro:
+        return jsonify({"erro": "Token não encontrado"}), 400
+
+    if registro["token"] != token:
+        return jsonify({"erro": "Token inválido"}), 400
+
+    if datetime.now() > registro["expira"]:
+        return jsonify({"erro": "Token expirado"}), 400
+
+    try:
+        user_obj.update_password_by_email(email, new_password)
+        del tokenSaved[email]
+        return jsonify({"status": "Senha atualizada com sucesso"}), 200
+
+    except Exception as e:
+        return jsonify({"erro": str(e)}), 400    
