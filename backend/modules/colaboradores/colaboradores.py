2fda07751721d75111dfa55a2a9d4279770b0a90
Burhkardt <juliandrecesar@gmail.com>
2026-04-22 09:07:54 -0300

    resolvendo conflitos

--- backend/modules/colaboradores/colaboradores.py
+++
@@ -1,6 +0,0 @@
-from database.connection import meta
-from modules.crud.base import BaseCRUD
-
-class Colaborador(BaseCRUD):
-    def __init__(self):
-        super().__init__(meta.tables.get('colaboradores'))
--- backend/modules/usuarios/usuarios.py
+++
@@ -1,6 +0,0 @@
-from database.connection import meta
-from modules.crud.base import BaseCRUD
-
-class Usuario(BaseCRUD):
-    def __init__(self):
-        super().__init__(meta.tables.get('usuarios'))
