e57fb0466fccd09fba61b1dbdf55642cf63e2087
EduardoNstm <141466697+EduardoNstm@users.noreply.github.com>
2026-04-23 11:44:51 -0300

    Revert "testando ci/cd"

--- backend/config/settings.py
+++
@@ -1,7 +0,0 @@
-import os
-
-DB_HOST = os.getenv("MYSQL_HOST", "localhost")
-DB_PORT = int(os.getenv("MYSQL_PORT", 3306))
-DB_NAME = os.getenv("MYSQL_DATABASE", "patrimonio")
-DB_USER = os.getenv("MYSQL_USER", "root")
-DB_PASS = os.getenv("MYSQL_PASSWORD", "")
--- backend/register.py
+++
@@ -1,6 +0,0 @@
-from modules.user.user_routes import user_bp
-from modules.authentication.auth_routes import auth_bp
-
-def register_routes(app):
-    app.register_blueprint(user_bp)
-    app.register_blueprint(auth_bp)
