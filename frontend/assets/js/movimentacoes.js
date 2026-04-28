3898e7926f2c1723865bb7092b70a08753a0e5b8
Leandra Michely <design.lemi@gmail.com>
2026-04-27 21:04:48 -0300

    rota pagina movimentações

--- frontend/assets/js/movimentacoes.js
+++ frontend/assets/js/movimentacoes.js
@@ -25,3 +25,76 @@ botaoAplicar.addEventListener('click', () => {
         // Fazer a integração aqui para buscar no banco de dados e jogar no HTML
     }
 })
+
+
+
+
+/////// TABELA DE MANUTRENÇÕES POR ID
+
+document.addEventListener("DOMContentLoaded", () => {
+    carregarManutencoes();
+});
+
+async function carregarManutencoes() {
+    try {
+        const resposta = await fetch("http://localhost:3000/api/manutencoes");
+        const dados = await resposta.json();
+
+        const tabela = document.getElementById("tabela-manutencoes");
+        tabela.innerHTML = "";
+
+        dados.forEach(manutencao => {
+
+            const linha = `
+                <tr style="border-bottom: 1px solid #f1f1f1;">
+                    
+                    <td style="color: #1D4587; padding-top: 15px; padding-bottom: 15px;">
+                        <a href="#" 
+                        style="color: #1D4587; text-decoration: none; font-weight: 500;">
+                        ${String(manutencao.id).padStart(7, '0')}
+                        </a>
+                    </td>
+
+                    <td style="color: #1D4587; padding-top: 15px; padding-bottom: 15px; text-align: center;">
+                        <a href="#" 
+                        style="color: #1D4587; text-decoration: none; font-weight: 500;">
+                        ${manutencao.nome_equipamento}
+                        </a>
+                    </td>
+
+                    <td class="text-end">
+                        <button class="btn btn-sm btn-outline-danger px-2 py-1 btn-info"
+                                data-id="${manutencao.id}">
+                            <i class="bi bi-info"></i>
+                        </button>
+                    </td>
+
+                </tr>
+            `;
+
+            tabela.innerHTML += linha;
+        });
+
+        adicionarEventos();
+
+    } catch (erro) {
+        console.error("Erro ao carregar manutenções:", erro);
+    }
+}
+
+
+
+/////// BOTÕES DE CADA MANUTENÇÃO
+
+
+function adicionarEventos() {
+
+    // INFO
+    document.querySelectorAll(".btn-info").forEach(botao => {
+        botao.addEventListener("click", () => {
+            const id = botao.getAttribute("data-id");
+
+            window.location.href = `movimentacoes-info.html?id=${id}`;
+        });
+    });
+}
