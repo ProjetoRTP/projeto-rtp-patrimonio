632fd1a4535ca839900f9cd90c5e143fa096a778
Juliandre César <juliandrecesar@gmail.com>
2026-05-05 09:05:01 -0300

    Criação da tabela de equipamentos genéricos e das classes relacionadas

---
+++ backend/modules/equipments/classes/generic_type.py
@@ -0,0 +1,20 @@
+from database.connection import meta, engine
+from modules.crud.base import BaseCRUD
+from sqlalchemy import update
+
+class Generic_type(BaseCRUD):
+    def __init__(self):
+        table = meta.tables.get('tipo_generico')
+
+        if table is None:
+            raise Exception("Tabela 'tipo_generico' não encontrada no metadata")
+
+        super().__init__(table)
+
+    def change_status(self, id, status):
+        with engine.begin() as conn:
+            conn.execute(
+                update(self.table)
+                .where(self.table.c.id == id)
+                .values(status=status)
+            )
